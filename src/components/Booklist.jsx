cab5e9382f2f34c92b5e3b75ea881fbdb16591e3
Newtayo <akandeabdul@gmail.com>
2023-04-06 05:25:41 +0100

    Implement add and remove books functionality

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
+
+import { useSelector } from 'react-redux';
 import Bookcard from './Bookcard';
 import AddBook from './AddBook';
 
 const Booklist = () => {
-  const booklibrary = [{
-    id: uuidv4(),
-    category: 'Action',
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    percent: '60%',
-
-  }, {
-    id: uuidv4(),
-    category: 'Action',
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    percent: '60%',
-
-  }];
+  const { bookslibrary } = useSelector((state) => state.book);
 
   return (
     <div>
-      { booklibrary.map((books) => (
+      { bookslibrary.map((books) => (
 
         <Bookcard key={books.id} book={books} />
       ))}
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  bookslibrary: [],
+  bookslibrary: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
   bookTitle: '',
   author: '',
 };
@@ -11,14 +30,17 @@ const booksSlice = createSlice({
   initialState,
 
   reducers: {
-    addBook: (state, { payload: { bookTitle, author } }) => {
-      state.books.push({ bookTitle, author });
+    addBook: (state, action) => {
+      state.bookslibrary.push(action.payload);
     },
-    removeBook: () => { },
-  },
+    removeBook: (state, action) => ({
+      ...state, bookslibrary: state.bookslibrary.filter((book) => book.item_id !== action.payload),
 
+    }),
+
+  },
 });
 
-export const { addBook } = booksSlice.actions;
+export const { addBook, removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
